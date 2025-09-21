7154f5610160b76593ce4fceb15d11d93271a2ba
Shinjuuichi <sinlthai570@gmail.com>
2025-09-21 20:53:11 +0700

    Done for ocelot

--- ApiGateway/Program.cs
+++ ApiGateway/Program.cs
@@ -1,13 +1,21 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.AddServiceDefaults();
 
+builder.Configuration
+    .SetBasePath(builder.Environment.ContentRootPath)
+    .AddOcelot(); // single ocelot.json file in read-only mode
+builder.Services
+    .AddOcelot(builder.Configuration);
 
 var app = builder.Build();
 
 app.MapDefaultEndpoints();
 
 app.UseHttpsRedirection();
-
+await app.UseOcelot();
 
 app.Run();
